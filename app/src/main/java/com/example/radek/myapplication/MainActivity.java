b09c52964ec02f194366ac6630c3c755c6eea53b
Radoslaw Sobolak <radoslaw.sobolak@interia.pl>
2018-11-25 19:13:40 +0100

    Create simple application

---
+++ app/src/main/java/com/example/radek/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.radek.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onRegisterButtonClick(View v)
+    {
+        setContentView(R.layout.activity_register);
+    }
+
+}
